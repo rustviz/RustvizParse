f87029723e9564169a1957caf64d890e8038e384
jesssleepy <panyu@umich.edu>
2021-07-01 16:59:58 +0800

    can detect macro and treat that as a function now

--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,17 @@
 fn main() {
     let mut b = 5;
     let c = &b;
-    let & a = c;
+    let &_a = c;
+    b = 2;
     let s = String::from("hello");
     let len1 = String::len(&s);
     let len2 = s.len(); // shorthand for the above
+    assert!(b!=3);
     println!("len1 = {} = len2 = {}", len1, len2);
     let x = String::from("hello");
-    let z = {
+    let _z = {
         let y = x;
-        println("{}", y);
+        println!("{}", y);
         // ...
     };
     println!("Hello, world!");
