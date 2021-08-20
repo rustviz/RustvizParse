de4863dd8c514b1481f6048873d776256155b02d
Haochen Zhang <haochenz@umich.edu>
2021-08-20 18:50:49 +0800

    Revert "added stack info gather"
    This reverts commit 635a830b5b62984bfcca98f01765aa4514ea307a.

--- src/test.rs
+++ src/test.rs
@@ -1,18 +1,11 @@
-// fn main() {
-//     let s = String::from("hello");
-//     takes_ownership(s);
-//     let mut x = 5;
-//     let y = x;
-//     x = 6;
-// }
-
-// fn takes_ownership(some_string: String) {
-//     println!("{}", some_string);
-// }
-
 fn main() {
-    fn test_str() {
-        println!("test_str called");
-    }
-    test_str();
+    let s = String::from("hello");
+    takes_ownership(s);
+    let mut x = 5;
+    let y = x;
+    x = 6;
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
