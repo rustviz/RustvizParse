635a830b5b62984bfcca98f01765aa4514ea307a
Haochen Zhang <haochenz@umich.edu>
2021-08-20 18:49:00 +0800

    added stack info gather

--- src/test.rs
+++ src/test.rs
@@ -1,11 +1,18 @@
-fn main() {
-    let s = String::from("hello");
-    takes_ownership(s);
-    let mut x = 5;
-    let y = x;
-    x = 6;
-}
+// fn main() {
+//     let s = String::from("hello");
+//     takes_ownership(s);
+//     let mut x = 5;
+//     let y = x;
+//     x = 6;
+// }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+// fn takes_ownership(some_string: String) {
+//     println!("{}", some_string);
+// }
+
+fn main() {
+    fn test_str() {
+        println!("test_str called");
+    }
+    test_str();
 }
